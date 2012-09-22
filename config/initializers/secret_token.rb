3a1dfd2fdfc031c719135a7372a761ac21a689d8
joe <marholarnkij@hotmail.com>
2012-09-22 19:48:34 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DareApp::Application.config.secret_token = '84e0c0004ffcb45d5d8dc78ae7a826793be4d657df75ffd81717696236431421bdd0c7bf3f79f56eda325480b15d893fb224358d9cd714de6ab16a28fa83cf71'
